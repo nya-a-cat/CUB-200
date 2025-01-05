974c75044c64e94bb57c302381332242a9987629
nya-a-cat <188900516+nya-a-cat@users.noreply.github.com>
2025-01-05 17:10:46 +0800

    init

--- run.py
+++ run.py
@@ -14,9 +14,12 @@ train_transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     transforms.RandomRotation(10),
-    transforms.RandomCrop(10),
+    transforms.RandomCrop(224),
     transforms.RandomHorizontalFlip(),
     transforms.RandomVerticalFlip(),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # 添加颜色抖动
+    transforms.RandomAffine(degrees=0, translate=(0.1, 0.1), scale=(0.9, 1.1)),    # 添加仿射变换
+    transforms.RandomPerspective(distortion_scale=0.2, p=0.5),                      # 添加透视变换
 ])
 test_transform = transforms.Compose([
     transforms.Resize((256, 256)),
